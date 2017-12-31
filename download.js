936a672e8f7d5cdd978ca7809c4fe890b959920a
utkarsh867 <utkarsh867@gmail.com>
2017-12-31 23:18:54 +0530

    complete

---
+++ download.js
@@ -0,0 +1,39 @@
+const htpp = require('http');
+const fs = require('fs');
+const path = require('path');
+const uuid = require('uuid/v1');
+const downloadPage = (url='http://nodeprogram.com') =>{
+    const fetchPage = (urlF, callback) =>{
+        const folderName = uuid();
+        htpp.get(url,(response) => {
+            let buff='';
+            response.on('data', (chunk) =>{
+                buff+=chunk; 
+            });
+            response.on('end', ()=>{
+                callback(null,buff);
+            });
+            response.on('error', (error)=>{
+                console.log(error);
+                callback(error);
+            });
+        }).on('error', (error)=>{
+            console.log(error.message);
+            callback(error);
+        });
+    }
+    
+    fs.mkdirSync(folderName);
+    
+    fetchPage(url, (error, data) => {
+        if(error){
+            return console.log(error);
+        }
+
+        fs.writeFileSync(path.join(__dirname, folderName, 'url.txt'), url);
+        fs.writeFileSync(path.join(__dirname, folderName, 'file.html'), data);
+    
+        console.log('downloading is done in folder', folderName);
+    });
+}
+downloadPage(process.argv[2]);
